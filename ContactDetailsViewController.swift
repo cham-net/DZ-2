fdf904f66f9e9880d6ecd0aad117fdb527c59d20
Maxim Vitovitsky <maxim@twincorp.ru>
2019-11-14 01:45:25 +0500

    Review.
    В основном проблема в редактировании файлов, по всему проекту стоит настройка на 2 пробела в табе, вместо одного

--- ContactDetailsViewController.swift
+++ ContactDetailsViewController.swift
@@ -1,70 +1,75 @@
 import UIKit
 
 class ​ContactDetailsViewController: UITableViewController {
-    
-    var contact: Contact?
-  
-    var group = "" {
-      didSet {
-        DetailLabel.text = group
-      }
-    }
-    
-    var imagePicker: ImagePicker!
-  
-  @IBOutlet weak var FirstNameTextField: UITextField!
-  @IBOutlet weak var LasttNameTextField: UITextField!
-  @IBOutlet weak var PhoneNumberTextField: UITextField!
-  @IBOutlet weak var DetailLabel: UILabel!
-  @IBOutlet weak var PhotoView: UIImageView!
-  @IBAction func PhotoSeletion(_ sender: Any) {
-    self.imagePicker.present(from: sender as! UIView)
-  }
-  
-  required init?(coder aDecoder: NSCoder) {
-    print("init ContactDetailsViewController")
-    super.init(coder: aDecoder)
-  }
-  deinit {
-    print("deinit ContactDetailsViewController")
-  }
-  
-    override func viewDidLoad() {
-      group = "Одноклассники"
-      FirstNameTextField.becomeFirstResponder()
-      self.imagePicker = ImagePicker(presentationController: self, delegate: self)
-    }
-  
-  override func prepare(for segue: UIStoryboardSegue, sender: Any?)  {
-    
-    if segue.identifier == "SaveGroup",
-          let contactFirstName = FirstNameTextField.text,
-          let contactLastName = LasttNameTextField.text,
-          let contactPhoneNumber = PhoneNumberTextField.text,
-          let groupName = DetailLabel.text,
-          let photo = PhotoView.image {
-      contact = Contact(firstName: contactFirstName, lastName: contactLastName, contactGroup: groupName, phoneNumber: contactPhoneNumber, photo: photo, deleted: false)
-       }
-    if segue.identifier == "ChooseGroup",
-       let groupsViewController = segue.destination as? ​GroupsViewController {
-      groupsViewController.groupsDataSource.selectedGroup = group
-    }
-  }
+	
+	var contact: Contact?
+	
+	var group = "" {
+		didSet {
+			DetailLabel.text = group
+		}
+	}
+	
+	var imagePicker: ImagePicker!
+	
+	// Все аутлеты, так же как переменные, должны быть с маленькой буквы
+	@IBOutlet weak var FirstNameTextField: UITextField!
+	@IBOutlet weak var LasttNameTextField: UITextField!
+	@IBOutlet weak var PhoneNumberTextField: UITextField!
+	@IBOutlet weak var DetailLabel: UILabel!
+	@IBOutlet weak var PhotoView: UIImageView!
+	@IBAction func PhotoSeletion(_ sender: Any) {
+		self.imagePicker.present(from: sender as! UIView)
+	}
+	
+	// Если это только для тестов и логгов, то после проверки
+	// лучше было бы удалить
+	required init?(coder aDecoder: NSCoder) {
+		print("init ContactDetailsViewController")
+		super.init(coder: aDecoder)
+	}
+	deinit {
+		print("deinit ContactDetailsViewController")
+	}
+	
+	override func viewDidLoad() {
+//		Лучше тут задавать не просто строку, а брать
+//		первый элемент из GroupsDataSource().groups
+		group = "Одноклассники"
+		FirstNameTextField.becomeFirstResponder()
+		self.imagePicker = ImagePicker(presentationController: self, delegate: self)
+	}
+	
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?)  {
+		
+		if segue.identifier == "SaveGroup",
+			let contactFirstName = FirstNameTextField.text,
+			let contactLastName = LasttNameTextField.text,
+			let contactPhoneNumber = PhoneNumberTextField.text,
+			let groupName = DetailLabel.text,
+			let photo = PhotoView.image {
+			contact = Contact(firstName: contactFirstName, lastName: contactLastName, contactGroup: groupName, phoneNumber: contactPhoneNumber, photo: photo, deleted: false)
+		}
+		if segue.identifier == "ChooseGroup",
+			let groupsViewController = segue.destination as? ​GroupsViewController {
+			groupsViewController.groupsDataSource.selectedGroup = group
+		}
+	}
 }
 
 extension ​ContactDetailsViewController {
-  @IBAction  func  SelectedGroup (segue: UIStoryboardSegue) {
-    if let groupViewController = segue.source as? ​GroupsViewController,
-       let selectedGame = groupViewController.groupsDataSource.selectedGroup {
-      group = selectedGame
-    }
-  }
+	@IBAction  func  SelectedGroup (segue: UIStoryboardSegue) {
+		if let groupViewController = segue.source as? ​GroupsViewController,
+			let selectedGame = groupViewController.groupsDataSource.selectedGroup {
+			group = selectedGame
+		}
+	}
 }
 
 extension ​ContactDetailsViewController: ImagePickerDelegate {
-  func didSelect(image: UIImage?) {
-      self.PhotoView.image = image
-  }
+	func didSelect(image: UIImage?) {
+		self.PhotoView.image = image
+	}
 }
 
 
